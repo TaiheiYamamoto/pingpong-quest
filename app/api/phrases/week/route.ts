6ffb1aa3005c8ef901c0c56145405693698d286a
Taihei Yamamoto <yamamototaihei55836317@gmail.com>
2025-10-29 16:38:20 +0900

    fix: disable eslint ban-ts-comment rule for phrases/week

--- app/api/phrases/week/route.ts
+++ app/api/phrases/week/route.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable */
 // app/api/phrases/week/route.ts
 import type { NextRequest } from "next/server";
 
